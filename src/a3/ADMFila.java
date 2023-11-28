e47d9c0470b89641913ee156c12a03700d7413e7
djalmafilho <djalmafilho.0510@aluno.saojudas.br>
2023-11-28 13:03:19 -0300

    parte 2 vercao 1

---
+++ src/a3/ADMFila.java
@@ -0,0 +1,111 @@
+package a3;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ADMFila {
+
+    private FilaBr filaBr;
+
+    public ADMFila() {
+    }
+
+    // checa se tem um ou mais amigos e insere na melhor fila
+    public void chegou(String nome, ListaGrupos listaGrupos) {
+        FilaBr current = filaBr;
+        ArrayList<Integer> opcoes = new ArrayList<>();
+        // encontra melhor posi;~ao dentro das filas
+        try {
+            ArrayList<String> conhecidos = listaGrupos.conhecidos(nome);
+            do {
+                int opcao = current.getLength();
+                for (int i = 0; i < conhecidos.size(); i++) {
+                    int posicao = current.posicaoAmigo(conhecidos.get(i));
+                    if (posicao < 0)
+                        posicao = current.getLength();
+                    if (opcao > posicao)
+                        opcao = posicao;
+                }
+                opcoes.add(opcao);
+                current = current.getNextFila();
+            } while (current != null);
+            // resgata a melhor opcao para inserir
+            int index = opcoes.indexOf(Collections.min(opcoes));
+            current = this.filaBr;
+            for (int i = 0; i < index; i++) {
+                current = current.getNextFila();
+
+            }
+            // encotra qual o amigo em melhor posi;'ao na mesma fila'
+            NodeNome melhorAmigo = null;
+            int melhorOpcao = current.getLength();
+            for (int i = 0; i < conhecidos.size(); i++) {
+                int posicao = current.posicaoAmigo(conhecidos.get(i));
+                if (posicao < 0) {
+                    posicao = current.getLength();
+                }
+                if (melhorOpcao > posicao) {
+                    melhorOpcao = posicao;
+                    melhorAmigo = current.busca(conhecidos.get(i));
+                }
+            }
+            if (melhorAmigo != null) {
+                current.incluiMeio(melhorAmigo, nome);
+                return;
+            }
+            current.incluiFinal(nome);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+
+            FilaBr menorfila = this.filaBr;
+            current = this.filaBr;
+            do {
+                menorfila = current.getLength() < menorfila.getLength() ? current : menorfila;
+                current = current.getNextFila();
+            } while (current != null);
+            menorfila.incluiFinal(nome);
+
+        }
+    }
+
+    public void criaFila(String ID) {
+        if (filaBr == null)
+            filaBr = new FilaBr(ID);
+        else
+            filaBr.criaFila(ID);
+    }
+
+    public void atendeFila(String ID) {
+        FilaBr current = this.filaBr;
+        do {
+            if (current.getID().equals(ID)) {
+                current.atendeFila();
+            }
+            current = current.getNextFila();
+        } while (current != null);
+
+    }
+
+    public void desiste(String nome) {
+        FilaBr current = this.filaBr;
+        do {
+            NodeNome desistente = current.busca(nome);
+            if (desistente != null) {
+                current.desiste(desistente);
+            }
+            current = current.getNextFila();
+        } while (current != null);
+
+    }
+
+    public void imprime() {
+        FilaBr current = this.filaBr;
+        do {
+            current.imprime();
+            current = current.getNextFila();
+        } while (current != null);
+    }
+
+}
--- src/a3/FilaBr.java
+++ src/a3/FilaBr.java
@@ -2,63 +2,140 @@ package a3;
 
 public class FilaBr {
 
-    private NodeNome front, rear;
+    private String ID;
+    private FilaBr nextFila = null;
+    private NodeNome front/* , rear */;
     private int length;
 
-    public FilaBr() {
-        front = rear = null;
+    public FilaBr(String ID) {
+        this.ID = ID;
+        front = null;
         length = 0;
     }
 
+    public int getLength() {
+        return length;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void criaFila(String ID) {
+        if (nextFila == null)
+            nextFila = new FilaBr(ID);
+        else
+            nextFila.criaFila(ID);
+    }
+
+    public FilaBr getNextFila() {
+        return nextFila;
+    }
+
     public void incluiFinal(String elem) {
-        NodeNome aux = new NodeNome();
-        aux.setInfo(elem);
-        aux.setNext(null);
-        aux.setPrev(null);
+        NodeNome novo = new NodeNome();
+        novo.setInfo(elem);
+        NodeNome aux = front;
         if (front == null) {
-            rear = front = aux;
-            length = 1;
-        } else {
-            rear.setNext(aux);
-            aux.setPrev(rear);
-            rear = aux;
+            front = novo;
+            length++;
+            return;
+        }
+        do {
+            if (aux.getNext() == null) {
+                aux.setNext(novo);
+                length++;
+                return;
+            }
+            aux = aux.getNext();
+        } while (aux != null);
+    }
+
+    public void incluiMeio(NodeNome amigo, String elem) {
+        if (amigo.getNext() != null) {
+            NodeNome mane = amigo.getNext();
+            NodeNome malandro = new NodeNome(elem);
+            malandro.setNext(amigo.getNext());
+            mane.setPrev(malandro);
+            amigo.setNext(malandro);
+            malandro.setPrev(amigo);
+            length++;
+        }else{
+            NodeNome malandro = new NodeNome(elem);
+            amigo.setNext(malandro);
+            malandro.setPrev(amigo);
             length++;
         }
+
     }
-    public void incluiMeio( NodeNome amigo, String elem){
-        NodeNome mane = amigo.getNext();
-        NodeNome malandro = new NodeNome(elem);
-        malandro.setNext(amigo.getNext());
-        mane.setPrev(malandro);
-        amigo.setNext(malandro);
-        malandro.setPrev(amigo);
-        length++;
-    
-}
-    public NodeNome busca(String nome){
-        NodeNome aux =  front;
-        while(aux.getNext()!=null){
+
+    public NodeNome busca(String nome) {
+        NodeNome aux = front;
+        do {
             String termo = aux.getInfo();
-            if (termo.equals(nome)){
+            if (termo.equals(nome)) {
                 return aux;
             }
-            aux=aux.getNext();
+            aux = aux.getNext();
+        } while (aux != null);
+        return null;
+    }
+
+    public void desiste(NodeNome desiste) {
+        if (desiste.getPrev() == null) {
+            front = null;
+            return;
+        } else {
+
+            if (desiste.getNext() != null) {
+                NodeNome prev = desiste.getPrev();
+                prev.setNext(null);
+                length--;
+            } else {
+                NodeNome prev = desiste.getPrev();
+                NodeNome next = desiste.getNext();
+                prev.setNext(next);
+                next.setPrev(prev);
+                length--;
+            }
         }
-    return null;
+
     }
-    
-    public void desiste(NodeNome desiste){
-        NodeNome prev = desiste.getPrev();
-        NodeNome next = desiste.getNext();
-        prev.setNext(next);
-        next.setPrev(prev);
-        length--;
+
+    public void atendeFila() {
+        NodeNome aux = front;
+        if (aux.getNext() == null) {
+            return;
+        } else {
+            front = front.getNext();
+            length--;
+        }
+
     }
-    
-    public void atendeFila(){
-        NodeNome aux = rear.getNext();
-        front=rear= aux;
-        length--;
+
+    public int posicaoAmigo(String amigo) {
+        if (front == null)
+            return -1;
+        int posicaoAmigo = 1;
+        NodeNome pessoaFila = this.front;
+        do {
+            if (pessoaFila.getInfo().equals(amigo))
+                return posicaoAmigo;
+            posicaoAmigo++;
+            pessoaFila = pessoaFila.getNext();
+        } while (pessoaFila != null);
+
+        return posicaoAmigo;
     }
-    
+
+    public void imprime() {
+        NodeNome current = this.front;
+        System.out.print("#" + ID + " [ ");
+        while (current != null) {
+            System.out.print(current.getInfo() + ", ");
+            current = current.getNext();
+        }
+        System.out.println(" ]");
+    }
+
 }
